a4759d90a38baae5d96053e98f2494fdfd6ca322
camillia <145443140+camilliaa@users.noreply.github.com>
2024-12-13 11:33:24 +0100

    fix5

--- scripts/themeToggle.js
+++ scripts/themeToggle.js
@@ -1,15 +1,3 @@
-function themeToggle() {
-    const theme = getCookie('theme');
-    const themeStyle = document.getElementById('theme-style');
-
-    if (theme === 'dark') {
-        themeStyle.setAttribute('href', 'styles/dark.css');
-    } else {
-        themeStyle.setAttribute('href', 'styles/light.css');
-    }
-}
-
-document.querySelector('.switch').addEventListener('click', themeToggle);
 
 window.onload = function() {
     const theme = getCookie('theme');
@@ -20,8 +8,15 @@ window.onload = function() {
     }
 };
 
-const switchButton = document.querySelector('.switch');
+document.querySelector('.switch').addEventListener('click', themeToggle);
 
-switchButton.addEventListener('click', () => {
-    themeStyle.setAttribute('href', 'styles/dark.css');
-});
+function themeToggle() {
+    const theme = getCookie('theme');
+    const themeStyle = document.getElementById('theme-style');
+
+    if (theme === 'dark') {
+        themeStyle.setAttribute('href', 'styles/dark.css');
+    } else {
+        themeStyle.setAttribute('href', 'styles/light.css');
+    }
+}
